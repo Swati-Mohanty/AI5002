f2d7dc3a4f91365ca5d27c059e3096376d52cc2c
Swati-Mohanty <70720621+Swati-Mohanty@users.noreply.github.com>
2021-01-30 22:38:17 +0530

    Update mgf.py

--- Assignment 1/codes/mgf.py
+++ Assignment 1/codes/mgf.py
@@ -1,3 +1,27 @@
+# Moment  
+from scipy import stats 
+import matplotlib.pyplot as plt
+import numpy as np  
+  
+arr1 = [[1, 3, 27],  
+        [3, 4, 6],  
+        [7, 6, 3],  
+        [3, 6, 8]]   
+arr2 = []
+arr2.append(stats.moment(arr1, moment = 0)) 
+arr2.append(stats.moment(arr1, moment = 2)) 
+
+print (arr2)
+plt.plot(arr2)
+print("Oth moment : \n", stats.moment(arr1, moment = 0)) 
+  
+print("\n6th moment : \n", stats.moment(arr1, moment = 6)) 
+  
+print("\n9th moment : \n", stats.moment(arr1, moment = 9, axis = None)) 
+  
+print("\n12th moment : \n", stats.moment(arr1, moment = 12, axis = 1)) 
+  
+print("\n10th moment : \n", stats.moment(arr1, moment = 10, axis = 1)) 
 #1-Dimensional Data:
 
 #Import required libraries:
