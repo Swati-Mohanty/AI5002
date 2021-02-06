e2d31ac1fffd7b556b5ff39641a1e11d6fc8e205
Swati-Mohanty <70720621+Swati-Mohanty@users.noreply.github.com>
2021-02-06 18:12:57 +0530

    Update bernoullidist.py

--- Assignment 2/codes/bernoullidist.py
+++ Assignment 2/codes/bernoullidist.py
@@ -11,3 +11,22 @@ print ("Mean =", mean)
 #Variance
 var=bernoulli.var(p)
 print ("Variance =", var)
+
+#Generating random variables 
+p=0.7
+a=0
+r = bernoulli.rvs(p, size=1000)
+#print(r)
+for i in range(1000):
+  if(r[i]==1):
+    a+=1
+a= a/1000
+print(a)
+#Mean
+mean=bernoulli.mean(a)
+print("Generated samples ")
+print ("Mean =", mean)
+
+#Variance
+var=bernoulli.var(a)
+print ("Variance =", var)
