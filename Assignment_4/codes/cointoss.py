f3db958f22876fdb438fa0f6292d13a1cc38823f
Swati-Mohanty <70720621+Swati-Mohanty@users.noreply.github.com>
2021-02-28 13:29:06 +0530

    Update cointoss.py

--- Assignment_4/codes/cointoss.py
+++ Assignment_4/codes/cointoss.py
@@ -1,28 +1,47 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import random
+import matplotlib.pyplot as plt 
 from scipy.stats import bernoulli
 
-#Simlen
-simlen=1000
+sample_size = 10000
 
-#Number of toss
-n =  3
+#Bernoulli simulation
+x1 = bernoulli.rvs(size=sample_size,p=0.5)
+x2 = bernoulli.rvs(size=sample_size,p=0.5)
+x3 = bernoulli.rvs(size=sample_size,p=0.5)
+x4 = bernoulli.rvs(size=sample_size,p=0.5)
 
-#Probability of getting H/T
-p = 0.5
-k=1
+y1 = x1+x2
+y2 = x1+x2+x3
+y3 = x1+x2+x3+x4
 
-#Simulating the probability using  the bernoulli random variable
-data_bern_mat = bernoulli.rvs(p,size=(n,simlen))
-data_binom=np.sum(data_bern_mat, axis=0)
-#print(data_bern_mat)
-#print(data_binom)
+#print(y1)
+#print(y2)
+#print(y3)
 
-probs_100 = [np.equal(data_binom,i).mean() for i in range(n+1)]
-print(probs_100)
-plt.xticks(range(n+1))
-plt.plot(list(range(n+1)), probs_100, color='blue', marker='o')
-plt.xlabel('Number of Heads',fontsize=14)
-plt.ylabel('Probability',fontsize=14)
-plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-plt.grid()
+#2 toss
+prob_y1 = [np.equal(y1,i).mean() for i in range(3)]
+print(prob_y1)
+#3 toss
+prob_y2 = [np.equal(y2,i).mean() for i in range(4)]
+print(prob_y2)
+#4 toss#
+prob_y3 = [np.equal(y3,i).mean() for i in range(5)]
+print(prob_y3)
+
+#Binomial simulation
+print('Binomial simulation')
+y11=np.random.binomial(n=2, p=0.5, size=sample_size)
+#print (y11)
+prob_y11 = [np.equal(y11,i).mean() for i in range(3)]
+print(prob_y11)
+
+y12=np.random.binomial(n=3, p=0.5, size=sample_size)
+#print (y12)
+prob_y12 = [np.equal(y12,i).mean() for i in range(4)]
+print(prob_y12)
+
+y13=np.random.binomial(n=4, p=0.5, size=sample_size)
+#print (y13)
+prob_y13 = [np.equal(y13,i).mean() for i in range(5)]
+print(prob_y13)
